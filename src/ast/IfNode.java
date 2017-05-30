b4e71d6030455d13a76dc2c79bbf4eafe2420864
Stefn93 <stefan.romanazzi@gmail.com>
2017-05-30 15:40:39 +0200

    BOH

--- src/ast/IfNode.java
+++ src/ast/IfNode.java
@@ -44,7 +44,7 @@ public class IfNode implements Node {
   public Node typeCheck() {
     if (!(FOOLlib.isSubtype(cond.typeCheck(),new BoolTypeNode()))) {
       System.out.println("non boolean condition in if");
-      System.exit(0);
+        System.exit(0);
     }
     Node t = th.typeCheck();
     Node e = el.typeCheck();
