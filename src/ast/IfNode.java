c37294c1cea37fc767d7132848cb1ad07abbc8ab
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2017-06-26 12:19:46 +0200

    Tipo Classe

--- src/ast/IfNode.java
+++ src/ast/IfNode.java
@@ -43,18 +43,18 @@ public class IfNode implements Node {
   
   public Node typeCheck() {
     if (!(FOOLlib.isSubtype(cond.typeCheck(),new BoolTypeNode()))) {
-      System.out.println("non boolean condition in if");
-        System.exit(0);
-    }
-    Node t = th.typeCheck();
-    Node e = el.typeCheck();
-    if (FOOLlib.isSubtype(t,e)) 
-      return e;
-    if (FOOLlib.isSubtype(e,t))
-      return t;
-    System.out.println("Incompatible types in then else branches");
-    System.exit(0);
-    return null;
+          System.out.println("non boolean condition in if");
+          System.exit(0);
+      }
+      Node t = th.typeCheck();
+      Node e = el.typeCheck();
+      if (FOOLlib.isSubtype(t,e))
+          return e;
+      if (FOOLlib.isSubtype(e,t))
+          return t;
+      System.out.println("Incompatible types in then else branches");
+      System.exit(0);
+      return null;
   }
   
   public String codeGeneration() {
