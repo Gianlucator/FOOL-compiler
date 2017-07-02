2b0485e555bc1e698720602c08e3f3dcb0195c78
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2017-07-02 19:08:40 +0200

    (NEW) mutual recursion between methods

--- src/ast/FunNode.java
+++ src/ast/FunNode.java
@@ -52,7 +52,7 @@ public class FunNode implements Node {
         STentry entry = new STentry(env.getNestingLevel(), env.getOffset()); //separo introducendo "entry"
 
         // se la st contiene già l'id ==> error
-        if (hm.put(id, entry) != null) {
+        if (hm.put(id, entry) != null && self == null) {
             res.add(new SemanticError("Fun id " + id + " already declared"));
         } else {
             // se la st non contiene ancora l'id ==> lo si aggiunge e
