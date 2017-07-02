9eff029675f4f34bfa215d250b95ec1be30c2c72
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2017-07-02 22:15:23 +0200

    (UPDATE) started adding new tests

--- tests_asm/test1.asm
+++ tests_asm/test1.asm
@@ -1,10 +1,35 @@
 push 0
-push 2
-push -3
+push 4
+push function0
+lfp
+push 4
+lfp
+push -4
 lfp
 add
 lw
-push 2
-mult
+js
 print
 halt
+
+function0:
+cfp
+lra
+push -3
+lfp
+lw
+add
+lw
+push 1
+lfp
+add
+lw
+add
+srv
+sra
+pop
+pop
+sfp
+lrv
+lra
+js
