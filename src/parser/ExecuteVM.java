3eeae641124bda86af67ad321875f4da0ce883c1
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2017-07-05 20:21:59 +0200

    (UPDATE) try catch in ExecuteVM

--- src/parser/ExecuteVM.java
+++ src/parser/ExecuteVM.java
@@ -2,6 +2,8 @@ package parser;
 
 import util.VMResult;
 
+import java.util.Arrays;
+
 public class ExecuteVM {
 
     public static final int CODESIZE = 10000;
@@ -26,100 +28,104 @@ public class ExecuteVM {
     }
 
     public void cpu() {
-        while (true) {
-            int bytecode = code[ip++]; // fetch
-            int v1, v2;
-            int address;
-            switch (bytecode) {
-                case SVMParser.PUSH:
-                    push(code[ip++]);
-                    break;
-                case SVMParser.POP:
-                    pop();
-                    break;
-                case SVMParser.ADD:
-                    v1 = pop();
-                    v2 = pop();
-                    push(v2 + v1);
-                    break;
-                case SVMParser.MULT:
-                    v1 = pop();
-                    v2 = pop();
-                    push(v2 * v1);
-                    break;
-                case SVMParser.DIV:
-                    v1 = pop();
-                    v2 = pop();
-                    push(v2 / v1);
-                    break;
-                case SVMParser.SUB:
-                    v1 = pop();
-                    v2 = pop();
-                    push(v2 - v1);
-                    break;
-                case SVMParser.STOREW: //
-                    address = pop();
-                    memory[address] = pop();
-                    break;
-                case SVMParser.LOADW: //Devo aver già pushato sullo stack l'address da cui caricare la variabile
-                    push(memory[pop()]);
-                    break;
-                case SVMParser.BRANCH:
-                    address = code[ip];
-                    ip = address;
-                    break;
-                case SVMParser.BRANCHEQ: //
-                    address = code[ip++];
-                    v1 = pop();
-                    v2 = pop();
-                    if (v2 == v1) ip = address;
-                    break;
-                case SVMParser.BRANCHLESSEQ:
-                    address = code[ip++];
-                    v1 = pop();
-                    v2 = pop();
-                    if (v2 <= v1) ip = address;
-                    break;
-                case SVMParser.JS: //
-                    address = pop();
-                    ra = ip;
-                    ip = address;
-                    break;
-                case SVMParser.STORERA: //
-                    ra = pop();
-                    break;
-                case SVMParser.LOADRA: //
-                    push(ra);
-                    break;
-                case SVMParser.STORERV: //
-                    rv = pop();
-                    break;
-                case SVMParser.LOADRV: //
-                    push(rv);
-                    break;
-                case SVMParser.LOADFP: //
-                    push(fp);
-                    break;
-                case SVMParser.STOREFP: //
-                    fp = pop();
-                    break;
-                case SVMParser.COPYFP: //
-                    fp = sp;
-                    break;
-                case SVMParser.STOREHP: //
-                    hp = pop();
-                    break;
-                case SVMParser.LOADHP: //
-                    push(hp);
-                    break;
-                case SVMParser.PRINT:
-                    System.out.println((sp < MEMSIZE) ? memory[sp] : "Empty stack!");
-                    break;
-                case SVMParser.HALT:
-                    //printMemory(PRINT_HEAP);
-                    //printMemory(PRINT_STACK);
-                    return;
+        try {
+            while (true) {
+                int bytecode = code[ip++]; // fetch
+                int v1, v2;
+                int address;
+                switch (bytecode) {
+                    case SVMParser.PUSH:
+                        push(code[ip++]);
+                        break;
+                    case SVMParser.POP:
+                        pop();
+                        break;
+                    case SVMParser.ADD:
+                        v1 = pop();
+                        v2 = pop();
+                        push(v2 + v1);
+                        break;
+                    case SVMParser.MULT:
+                        v1 = pop();
+                        v2 = pop();
+                        push(v2 * v1);
+                        break;
+                    case SVMParser.DIV:
+                        v1 = pop();
+                        v2 = pop();
+                        push(v2 / v1);
+                        break;
+                    case SVMParser.SUB:
+                        v1 = pop();
+                        v2 = pop();
+                        push(v2 - v1);
+                        break;
+                    case SVMParser.STOREW: //
+                        address = pop();
+                        memory[address] = pop();
+                        break;
+                    case SVMParser.LOADW: //Devo aver già pushato sullo stack l'address da cui caricare la variabile
+                        push(memory[pop()]);
+                        break;
+                    case SVMParser.BRANCH:
+                        address = code[ip];
+                        ip = address;
+                        break;
+                    case SVMParser.BRANCHEQ: //
+                        address = code[ip++];
+                        v1 = pop();
+                        v2 = pop();
+                        if (v2 == v1) ip = address;
+                        break;
+                    case SVMParser.BRANCHLESSEQ:
+                        address = code[ip++];
+                        v1 = pop();
+                        v2 = pop();
+                        if (v2 <= v1) ip = address;
+                        break;
+                    case SVMParser.JS: //
+                        address = pop();
+                        ra = ip;
+                        ip = address;
+                        break;
+                    case SVMParser.STORERA: //
+                        ra = pop();
+                        break;
+                    case SVMParser.LOADRA: //
+                        push(ra);
+                        break;
+                    case SVMParser.STORERV: //
+                        rv = pop();
+                        break;
+                    case SVMParser.LOADRV: //
+                        push(rv);
+                        break;
+                    case SVMParser.LOADFP: //
+                        push(fp);
+                        break;
+                    case SVMParser.STOREFP: //
+                        fp = pop();
+                        break;
+                    case SVMParser.COPYFP: //
+                        fp = sp;
+                        break;
+                    case SVMParser.STOREHP: //
+                        hp = pop();
+                        break;
+                    case SVMParser.LOADHP: //
+                        push(hp);
+                        break;
+                    case SVMParser.PRINT:
+                        System.out.println((sp < MEMSIZE) ? memory[sp] : "Empty stack!");
+                        break;
+                    case SVMParser.HALT:
+                        printMemory(PRINT_HEAP);
+                        //printMemory(PRINT_STACK);
+                        return;
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("A.o.o.b. at line " + e.getStackTrace()[0].getLineNumber());
         }
     }
 
@@ -157,6 +163,6 @@ public class ExecuteVM {
     }
 
     public VMResult getResult() {
-        return (sp < MEMSIZE) ? new VMResult(memory[sp], VMResult.OK) : new VMResult(0,VMResult.EMPTY_STACK);
+        return (sp < MEMSIZE) ? new VMResult(memory[sp], VMResult.OK) : new VMResult(0, VMResult.EMPTY_STACK);
     }
 }
